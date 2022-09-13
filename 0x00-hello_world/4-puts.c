56dcfa3512590263c70976d8c9412299fe569475
jnice1 <johnnice71@gmail.com>
2022-09-13 06:38:35 -0700

    put.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <Studio.h>
+   /**
+    *main-main entry point
+    * 
+    *return-return 0 after printingt the function
+    */
+int main (void)
+{puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
