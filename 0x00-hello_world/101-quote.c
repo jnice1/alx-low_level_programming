42294a04eb8f0a2b6a6931a09d9614f30f51f82c
jnice1 <johnnice71@gmail.com>
2022-09-14 06:38:37 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <studio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
-*
 *main - Entry point
 *
-*Return: Always 0 (success)
+*Return: Always 1 (success)
 *
 */
 int main(void)
 {
 char *mgs = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDOUT_FILENO,mgs, 59);
+write(STDOUT_FILENO, mgs, 59);
 return (1);
 }
