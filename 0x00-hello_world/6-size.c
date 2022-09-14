a3e8db39587ba3bb40086fa564848e3a99ca122e
jnice1 <johnnice71@gmail.com>
2022-09-14 05:47:41 -0700

    siize

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*
+*/
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float e;
+printf("Size of a char: %zu byte(s)\n", (unsigned long)size of (d));
+printf("Size of an int: %zu byte(s)\n", (unsigned long)size of (a));
+printf("Size of an long int: %zu byte(s)\n", (unsigned long)size of (b));
+printf("Size of an long long int: %zu byte(s)\n", (unsigned long)size of (c));
+printf("Size of an float: %zu byte(s)\n", (unsigned long)size of (e));
+return (0);
+}
