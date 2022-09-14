d3073ba5e4a2fc56a4b245cdc9975c52e38e707c
jnice1 <johnnice71@gmail.com>
2022-09-14 06:32:11 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+#include <stdlib.h>
+/**
+*
+*main - Entry point
+*
+*Return: Always 0 (success)
+*
+*/
+int main(void)
+{
+char *mgs = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDOUT_FILENO,mgs, 59);
+return (1);
+}
