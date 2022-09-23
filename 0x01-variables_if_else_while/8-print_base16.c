3713891df8dd19c96c074409a0a091ccc07eaff5
jnice1 <johnnice71@gmail.com>
2022-09-23 03:11:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ *
+ */
+int main(void)
+}
+int i = 0;
+char c = 'a';
+while (i <= 9)
+{
+putchar(i);
+i++;
+}
+while (c <= 'f')
+{
+putchar(c)
+c++
+}
+putchar('\n')
+}
+return (0);
+}
