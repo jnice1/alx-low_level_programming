ed7140a2946bd04f9bc257e0bbe5c8be5dc74f3f
jnice1 <johnnice71@gmail.com>
2022-09-23 02:01:04 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entyry point
+ *
+ *Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 9; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
